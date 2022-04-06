6fcfcc220c9f8cf534f2edb42e8a265677393189
hjseong <hjseong1222@gmail.com>
2022-04-06 11:54:03 +0900

    greenblogt-완

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,53 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
+    path: "/login",
+    name: "login",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
-  }
-]
+      return import(/* webpackChunkName: "about" */ "../views/LoginView.vue");
+    },
+  },
+  {
+    path: "/board",
+    name: "board",
+    component: function () {
+      return import("../views/BoardView.vue");
+    },
+    children: [
+      {
+        path: ":page",
+        component: function () {
+          return import("../components/PageComponent.vue");
+        },
+      },
+    ],
+  },
+  {
+    path: "/user/:user",
+    name: "user",
+    component: function () {
+      return import("../views/UserView.vue");
+    },
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
